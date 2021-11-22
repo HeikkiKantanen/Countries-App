2525aed826f077c8aa1b8797438a5f61a7d99bf8
HeikkiKantanen <hessu.kantanen@gmail.com>
2021-11-22 15:12:18 +0200

    updated version

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from 'axios';
 
 
-function App() {
-  return (
-    <div>
-      
-       
-        
-     
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    data: [],
+  };
+
+  componentDidMount () {
+    axios
+    .get("https://restcountries.com/v3.1/all").then((res) => {
+      this.setState({ data: res.data });
+      console.log(this.state.data);
+    });
+  }
+
+
+  render () {
+    return (
+        <div className="countries">
+          {this.state.data.map((country) => (
+          <div className="country" key={country.name.common}>
+            <h2>{country.name.common}</h2> capital is <p>{country.capital}</p>
+            <img src={country.flags.png} />
+            {/* <p className="borders">Borders with: { country.borders }</p> */}
+          </div>
+          ))}
+        </div>
+      );
+    };
+};
 
 export default App;
